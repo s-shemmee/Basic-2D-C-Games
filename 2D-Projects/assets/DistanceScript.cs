b3db0b62689050b4766971bae434de5402707a23
s-shemmee <56132945+s-shemmee@users.noreply.github.com>
2022-08-09 15:29:02 +0100

    Add files via upload

---
+++ 2D-Projects/assets/DistanceScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceScript : MonoBehaviour {
+
+	public GameObject object1;
+	public GameObject object2;
+
+	void Start () {
+		Vector2 object1Coordinates = object1.transform.position;
+		Vector2 object2Coordinates = object2.transform.position;
+		float distance = Vector2.Distance (object1Coordinates, object2Coordinates);
+
+		Debug.Log ("the distance between the 2 objects is " + distance.ToString ());
+		
+	}
+
+}
