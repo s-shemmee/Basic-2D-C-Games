9466f76a003d87fb555a21b847fcb7659e036c15
s-shemmee <56132945+s-shemmee@users.noreply.github.com>
2022-08-09 21:40:51 +0100

    Add files via upload

---
+++ unity-math-project/assets/PhysicsGravity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsGravity : MonoBehaviour {
+
+	public Vector3 gravity;
+	// Use this for initialization
+	void Start () {
+		Physics.gravity = gravity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
